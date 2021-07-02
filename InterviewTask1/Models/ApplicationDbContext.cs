26a98ca57e8e3db5c6e88120e67276e5901dabb6
burhaneyuboglu <burhaneyuboglu@gmail.com>
2021-07-02 14:40:00 +0100

    InterviewTask1 modülünün ilk iadesi

---
+++ InterviewTask1/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InterviewTask1.Models;
+
+namespace InterviewTask1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDo> TodoItems { get; set; }
+
+    }
+}
---
+++ InterviewTask1/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InterviewTask1.Models
+{
+    public class ToDo
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
